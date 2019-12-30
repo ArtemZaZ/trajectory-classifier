afd58d642a1c28a65f00acc5fe8aa81388e9be54
ArtemZaZ <temka.911@mail.ru>
2019-12-31 00:19:51 +0300

    Поправил косяк с путями, убрал невалидные скрипты, добавил курсач в pdf

--- classificate.py
+++ classificate.py
@@ -4,9 +4,9 @@ from sklearn import neighbors
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-data = pd.read_pickle('done_logarifm.pkl')
-rec = pd.read_pickle('done_rect.pkl')
-line = pd.read_pickle('done_line.pkl')
+data = pd.read_pickle('datasets/done_logarifm.pkl')
+rec = pd.read_pickle('datasets/done_rect.pkl')
+line = pd.read_pickle('datasets/done_line.pkl')
 
 data['type'] = [0]*1000
 rec['type'] = [1]*1000
@@ -30,4 +30,4 @@ ytestpredict = knn.predict(xtest)
 print("test error: ", np.mean(ytest != ytestpredict)*100)
 test = data[data['type'] == 0]
 test = test.drop(['sx', 'sy', 'sz', 'type', 'dt'], axis=1)
-print(np.sum(knn.predict(test))/1000)
+# print(np.sum(knn.predict(test))/1000)
